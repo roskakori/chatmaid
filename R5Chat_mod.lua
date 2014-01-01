542398b8351201f2863a246ef83259d873c101c6
Thomas Aglassinger <roskakori@users.sourceforge.net>
2014-01-01 23:21:48 +0100

    Improved performance in case Chatmaid has been disabled in Options > Chat.

--- R5Chat_mod.lua
+++ R5Chat_mod.lua
@@ -10,13 +10,15 @@
 @after "\tif not IO_Settings.Channels[args.channel] then"
 @after "\tend"
 	-- TODO: only check if IO_Settings.General.ChatmaidEnabled is true.
-	text, action = sanitized(args.channel, args.text)
-	if action ~= nil then
-		if IO_Settings.General.ChatmaidExplainActions then
-			args.text = args.text.." ["..action.."]"
-		elseif not text then
-			return nil
-		else
-			args.text = text
+	if IO_Settings.General.ChatmaidEnabled then
+		text, action = sanitized(args.channel, args.text)
+		if action ~= nil then
+			if IO_Settings.General.ChatmaidExplainActions then
+				args.text = args.text.." ["..action.."]"
+			elseif not text then
+				return nil
+			else
+				args.text = text
+			end
 		end
 	end
