0b634d7578a1fb7c64402b94f7804a228ba006c4
Thomas Aglassinger <roskakori@users.sourceforge.net>
2014-01-01 05:12:08 +0100

    Added call to sanitize chat message as modification to R5Chat.lua.

---
+++ R5Chat_mod.lua
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+-- Modifications for Firefall's chat module to filter messages through
+-- chatmaid.
+@mod "chatmaid.lua"
+@after last glob "require \"?*\""
+@include "chatmaid.lua"
+
+@mod "sanitize message"
+@after "function OnChatMessage(args)"
+@after "\tif not IO_Settings.Channels[args.channel] then"
+@after "\tend"
+	-- TODO: only check if IO_Settings.General.ChatmaidEnabled is true.
+	text, action = sanitized(args.channel, args.text)
+	if action ~= nil then
+		if IO_Settings.General.ChatmaidExplainActions then
+			args.text = args.text.." ["..action.."]"
+		elseif not text then
+			return nil
+		else
+			args.text = text
+		end
+	end
